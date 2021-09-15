280351357076af6732bc56f90defe4542426b4e6
Pedro Torres <kpedro101@gmail.com>
2021-09-14 23:01:00 -0300

    Initial Marvel API Setup

---
+++ app/src/main/java/com/marvel/myapplication/apisetup/Authentication.java
@@ -0,0 +1,58 @@
+package com.marvel.myapplication.apisetup;
+
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class Authentication {
+
+    public static final String PRIVATE_KEY = "b41c80713718c560bba0ad8c1c9d9f2ad933d04a";
+    public static final String PUBLIC_KEY = "ddbc8dc8d16b38f901dcc60eb1c50fd6";
+    private long mTs;
+    private String mHash;
+    private static Calendar mCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+
+    public Authentication() {
+        create();
+    }
+
+    public void create() {
+        this.mTs = mCalendar.getTimeInMillis() / 1000L;
+        this.mHash = md5(String.valueOf(this.mTs) + PRIVATE_KEY + PUBLIC_KEY);
+    }
+
+    public long getmTs() {
+        return mTs;
+    }
+
+    public String getmHash() {
+        return mHash;
+    }
+
+    private String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
+
+            MessageDigest digest = java.security.MessageDigest.getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                StringBuilder h = new StringBuilder(Integer.toHexString(0xFF & aMessageDigest));
+                while (h.length() < 2)
+                    h.insert(0, "0");
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("Exception: ", e.getMessage());
+        }
+        return "";
+    }
+
+}
---
+++ app/src/main/java/com/marvel/myapplication/apisetup/Request.java
@@ -0,0 +1,50 @@
+package com.marvel.myapplication.apisetup;
+
+import static com.marvel.myapplication.apisetup.Authentication.PUBLIC_KEY;
+
+public class Request {
+        private String name;
+        private int limit;
+        private int offset;
+        private Authentication auth;
+
+        public Request() {
+            auth = new Authentication();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getLimit() {
+            return limit;
+        }
+
+        public void setLimit(int limit) {
+            this.limit = limit;
+        }
+
+        public int getOffset() {
+            return offset;
+        }
+
+        public void setOffset(int offset) {
+            this.offset = offset;
+        }
+
+        public String getHash() {
+            return auth.getmHash();
+        }
+
+        public String getTs() {
+            return String.valueOf(auth.getmTs());
+        }
+
+        public String getPublicKey() {
+            return PUBLIC_KEY;
+        }
+}
